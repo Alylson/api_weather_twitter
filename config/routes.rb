8ed2c7c684dbed44c096354e1d62c3f335d58f3a
vf <alylson@hotmail.com>
2022-02-27 20:54:59 -0300

    Refactor estrutura api rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/api/v1/weather', to: 'weather#index'
+  namespace :api do
+    namespace :v1 do
+      scope :weather do
+        get '/' => 'weather#index'
+      end
+    end
+  end
 end
