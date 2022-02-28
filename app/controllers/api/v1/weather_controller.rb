b315e985805c019f1977fe6873e1f31d884aa2e8
vf <alylson@hotmail.com>
2022-02-27 21:32:37 -0300

    Remoção de token de acesso da api do controller

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -5,12 +5,11 @@ module Api::V1
 
       query = {
         id: '2643743',
-        units: 'metric',
-        appid: '93fa643fa0fc8ce5599c5a8ea2e7898e',
+        units: 'metric'
       }
 
       client = HTTPClient.new
-      response = client.get('http://api.openweathermap.org/data/2.5/weather?id=2643743&lang=pt_br&appid=93fa643fa0fc8ce5599c5a8ea2e7898e', query)
+      response = client.get('http://api.openweathermap.org/data/2.5/weather?lang=pt_br', query)
       render json: response.body
     end
   end
